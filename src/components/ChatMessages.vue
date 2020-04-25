cd04e757a396aaf528959baffe9de8d44ac3c091
Rube <rubnvp@Rubens-MacBook-Pro.local>
2020-04-25 13:13:05 +0200

    firebase

---
+++ src/components/ChatMessages.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <h1>Messages:</h1>
+    <form @submit.prevent="sendMessage">
+      <input type="text" placeholder="Message" v-model="newMessage" />
+      <button type="submit">Send</button>
+    </form>
+    <ul>
+      <li v-for="message in messages" :key="message.id">{{ message.text }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+export default {
+  name: 'ChatMessages',
+  data: () => ({
+    $db: null,
+    newMessage: '',
+    messages: [],
+    unsubscribe: null,
+  }),
+  methods: {
+    sendMessage() {
+      this.$db.collection('messages')
+        .add({
+          text: this.newMessage,
+          date: new Date(),
+        })
+        .then(() => {
+          this.newMessage = '';
+        })
+    },
+  },
+  created() {
+    this.$db = firebase.firestore();
+    this.unsubscribe = this.$db.collection('messages')
+      .orderBy('date', 'desc')
+      .onSnapshot(snapshot => {
+        const messages = snapshot.docs
+          .map(doc => ({id: doc.id, ...doc.data()}));
+        this.messages = messages;
+      });
+    
+  },
+  destroyed() {
+    this.unsubscribe();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  /* display: inline-block; */
+  margin: 0 10px;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="login">
+    <p>
+      Email: <input type="text" v-model="email" />
+    </p>
+    <p>
+      Password: <input type="password" v-model="password" />
+    </p>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+export default {
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+  methods: {
+    login() {
+      firebase.auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() =>{
+          console.log('autenticado 👍');
+          this.$router.push({name: 'Home'});
+        })
+        .catch(() =>{
+          window.alert('Invalid email and password');
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>template
